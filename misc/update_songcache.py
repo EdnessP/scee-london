e2c7e951f8763e463a55e81ef0abff3aa840bd71
EdnessP <55930127+EdnessP@users.noreply.github.com>
2026-07-16 13:09:08 +0300

    scripts and patches

--- misc/update_songcache.py
+++ misc/update_songcache.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
-# Updates SongCache.xml with .pkg.drm files (!!! WIP, NOT YET DONE !!!)
-# Written by Edness   v0.1   2026-07-12
+# Updates SongCache.xml with .pkg.drm files
 
-import glob, os, xml.etree.cElementTree
+# Usage:
+#     python  update_songcache.py  "X:\path\to\songs"
+#   Optional:
+#     -o | --output <str> Path to the output file; can update an existing file
+#     -u | --user   <int> PS3 User ID (optional, leaving it as 0 also seems to work)
+#       python  update_songcache.py  "X:\path\to\songs"  -o "Y:\path\to\SongCache.xml"
+
+# Written by Edness   v1.0   2026-07-12 - 2026-07-16
+
+import glob, os, xml.etree.ElementTree
+
+def exists_prompt(output, prompt):
+    if os.path.exists(output):
+        while True:
+            response = input(f"{prompt} (Y/N): ")[:1].upper()
+            if response == "Y": return True
+            elif response == "N": return False
+            else: print("Error! Invalid response.")
+    return False
 
 def create_songcache(path, output=str(), user=int()):
     path = os.path.abspath(path)
@@ -10,18 +27,39 @@ def create_songcache(path, output=str(), user=int()):
         output = os.path.join(path, "SongCache.xml")
     output = os.path.abspath(output)
 
+    exist_song = list()
+    if exists_prompt(output, "Update existing output file?"):
+        songs = xml.etree.ElementTree.parse(output).getroot()
+        for song in songs:
+            assert len(song) == 6
+            assert song[0].tag == "CacheFolder"
+            assert song[1].tag == "Package"
+            assert song[2].tag == "Size"
+            assert song[3].tag == "PCL"
+            assert song[4].tag == "User"
+            assert song[5].tag == "IsNew"
+            assert len(song[0].text) == 15
+            assert song[0].text.startswith("Prod_Sku_")
+            assert len(song[1].text) == 20
+            assert song[1].text.startswith("Pack0_")
+            assert song[1].text.endswith(".pkg.drm")
+            exist_song.append(song[0].text[9:].upper())
+    else:
+        songs = xml.etree.ElementTree.Element("Songs")
+
     # this xml writer is kinda lame for various reasons, might be better to write it myself later
-    songs = xml.etree.cElementTree.Element("Songs")
-    for file in glob.iglob(os.path.join(glob.escape(path), "**", "Pack0_*.pkg.drm"), recursive=True):
+    for file in glob.iglob(os.path.join(glob.escape(path), "**", "Pack0_??????.pkg.drm"), recursive=True):
         name = os.path.split(file)[1]
-        song = xml.etree.cElementTree.SubElement(songs, "Song")
-        xml.etree.cElementTree.SubElement(song, "CacheFolder").text = f"Prod_Sku_{name[6:12]}"
-        xml.etree.cElementTree.SubElement(song, "Package").text = name
-        xml.etree.cElementTree.SubElement(song, "Size").text = str(os.path.getsize(file))
-        xml.etree.cElementTree.SubElement(song, "PCL").text = "1"
-        xml.etree.cElementTree.SubElement(song, "User").text = str(user)
-        xml.etree.cElementTree.SubElement(song, "IsNew").text = "False"
-    out = xml.etree.cElementTree.ElementTree(songs)
+        song_id = name[6:12]  # the same numerical id found internally but in base 36
+        if song_id.upper() in exist_song: continue
+        song = xml.etree.ElementTree.SubElement(songs, "Song")
+        xml.etree.ElementTree.SubElement(song, "CacheFolder").text = f"Prod_Sku_{song_id}"  # ideally should be grabbed from config.xml
+        xml.etree.ElementTree.SubElement(song, "Package").text = name
+        xml.etree.ElementTree.SubElement(song, "Size").text = str(os.path.getsize(file))  # unused? older games wrote it wrong anyway
+        xml.etree.ElementTree.SubElement(song, "PCL").text = "1"  # ideally should be grabbed from songs_#_0.xml
+        xml.etree.ElementTree.SubElement(song, "User").text = str(user)  # 0 seems to allow loading for any user
+        xml.etree.ElementTree.SubElement(song, "IsNew").text = "False"
+    out = xml.etree.ElementTree.ElementTree(songs)
     out.write(output, xml_declaration=True, encoding="UTF-8")
 
     print("Done! Output written to", output)
